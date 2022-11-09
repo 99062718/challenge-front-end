dc665617d3b3e8204cfe0a7f9c4ed45a288e131a
Tycho Hartingsveld <70889437+99062718@users.noreply.github.com>
2022-11-09 13:25:14 +0100

    added function changeVisibility

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log(parties)
+//document.getElementById("mainContainer").classList changed elements classes
+
+function changeVisibility(group, visibleOrHidden){
+    var elements = document.getElementsByClassName(group);
+    for(x = 0; x < elements.length; x++){
+        elements[x].style.visibility = visibleOrHidden;
+    }
+}
