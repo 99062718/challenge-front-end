e1454b7c07c03af9897473e8a726a905e3263137
Tycho Hartingsveld <70889437+99062718@users.noreply.github.com>
2022-11-09 13:51:33 +0100

    added a footer

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //document.getElementById("mainContainer").classList changed elements classes
 
+//used to show or remove groups of elements
 function changeVisibility(group, visibleOrHidden){
     var elements = document.getElementsByClassName(group);
     for(x = 0; x < elements.length; x++){
