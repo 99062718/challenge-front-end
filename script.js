fd9cb8c487d8e97957b9c9ca4e0661c29c36a758
Tycho Hartingsveld <70889437+99062718@users.noreply.github.com>
2022-11-11 08:24:43 +0100

    added logo and padding to mainContainer

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 //document.getElementById("mainContainer").classList changed elements classes
 
-//used to show or remove groups of elements
+//used to show or hide groups of elements
 function changeVisibility(group, visibleOrHidden){
     var elements = document.getElementsByClassName(group);
     for(x = 0; x < elements.length; x++){
         elements[x].style.visibility = visibleOrHidden;
     }
 }
+
+// changeVisibility("introScreen", "hidden");
