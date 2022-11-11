3bb372f7efb0df37c12925b4d590e953289ed8cd
Tycho Hartingsveld <70889437+99062718@users.noreply.github.com>
2022-11-11 13:52:04 +0100

    added answeredQuestion function

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 //document.getElementById("mainContainer").classList changed elements classes
 
+var currentQuestion = 0
+var partijScore = {
+    "VVD": 0,
+    "CDA": 0,
+    "PVV": 0,
+    "D66": 0,
+    "GroenLinks": 0,
+    "SP": 0,
+    "PvdA": 0,
+    "ChristenUnie": 0,
+    "Partij voor de Dieren": 0,
+    "SGP": 0,
+    "DENK": 0,
+    "Forum voor Democratie": 0,
+    "Lokaal in de kamer": 0,
+    "OndernemersPartij": 0,
+    "VNL": 0,
+    "Nieuwe Wegen": 0,
+    "De Burger Beweging": 0,
+    "Piratenpatij": 0,
+    "Artikel 1": 0,
+    "Libertarische Partij": 0,
+    "50Plus": 0,
+    "Vrijzinnige Partij": 0,
+    "Niet Stemmers": 0
+}
+
+parties.splice(parties.length - 2, 1);
+
 //mostly used to show or hide groups of elements
 function changeDisplay(group, displayType){
     var elements = document.getElementsByClassName(group);
@@ -13,12 +42,28 @@ function changeText(element, text){
     document.getElementById(element).innerHTML(text)
 }
 
-function questionGen(questionNum, topic, question){
-    questionGroup = "questionScreen";
-    changeText("questionText", questionNum = ". " + topic);
+//generates new question
+function questionGen(topic, question){
+    changeText("topicText", currentQuestion = ". " + topic);
     changeText("questionText", question);
 }
 
-document.getElementById("startButton").addEventListener("click", function(){ changeDisplay("introScreen", "none"); });
+//handles user answers and goes to next question
+function answeredQuestion(answer){
+    for(x = 0; x < subjects[currentQuestion]["parties"].length; x++){
+        if(subjects[currentQuestion]["parties"][x]["position"] == answer){
+            partijScore[subjects[currentQuestion]["parties"][x]["name"]]++;
+        }
+    }
+
+    currentQuestion++;
+    if(currentQuestion == subjects.length){
+
+    } else {
+        questionGen(subjects[currentQuestion]["title"], subjects[currentQuestion]["statement"]);
+    }
+}
+
+document.getElementById("startButton").addEventListener("click", function(){changeDisplay("introScreen", "none");});
 
 //changeDisplay("introScreen", "none");
