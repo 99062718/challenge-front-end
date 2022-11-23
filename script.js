70c6829582c35a4779e5a41fc0b7023590758a92
Tycho Hartingsveld <70889437+99062718@users.noreply.github.com>
2022-11-23 13:31:17 +0100

    fixed answer buttons not having respective answer attached

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ parties.splice(parties.length - 2, 1);
 //mostly used to show or hide groups of elements
 function changeDisplayClass(group, displayType){
     var elements = document.getElementsByClassName(group);
-    for(x = 0; x < elements.length; x++){
+    for(let x = 0; x < elements.length; x++){
         elements[x].style.display = displayType;
     }
 }
@@ -53,7 +53,7 @@ function questionGen(topic, question, stances){
     changeText("topicText", currentQuestion + 1 + ". " + topic);
     changeText("questionText", question);
 
-    for(x = 0; x < stances.length; x++){
+    for(let x = 0; x < stances.length; x++){
         document.getElementById(`${stances[x]["name"]} stance`).innerHTML = stances[x]["position"];
         document.getElementById(`${stances[x]["name"]} opinion`).innerHTML = stances[x]["opinion"];
     }
@@ -77,7 +77,7 @@ function answeredQuestion(answer){
     if(currentQuestion == subjects.length){
         changeDisplayClass("questionScreen", "none");
         changeDisplayClass("importanceScreen", "block");
-        for(x = 0; x < subjects.length; x++){
+        for(let x = 0; x < subjects.length; x++){
             if(subjects[x]["title"] in questionScore){
                 changeDisplayId(subjects[x]["title"], "block");
             } else {
@@ -90,7 +90,7 @@ function answeredQuestion(answer){
 }
 
 function finalScoreCalc(){
-    for(x = 0; x < subjects[currentQuestion]["parties"].length; x++){
+    for(let x = 0; x < subjects[currentQuestion]["parties"].length; x++){
         if(subjects[currentQuestion]["parties"][x]["position"] == answer){
             partijScore[subjects[currentQuestion]["parties"][x]["name"]]++;
         }
@@ -118,7 +118,7 @@ function goLastPage(){
 }
 
 //importance screen generation
-for(x = 0; x < subjects.length; x++){
+for(let x = 0; x < subjects.length; x++){
     var div = document.createElement("div");
     div.id = subjects[x]["title"];
 
@@ -139,7 +139,7 @@ for(x = 0; x < subjects.length; x++){
 for(key in partijScore){
     var toCreate = ["div", "p", "p", "div", "i", "p"];
     var elems = [];
-    for(x = 0; x < toCreate.length; x++){
+    for(let x = 0; x < toCreate.length; x++){
         elems.push(document.createElement(toCreate[x]));
     }
     
@@ -176,10 +176,9 @@ document.getElementById("startButton").addEventListener(
     }
 );
 
-var stances = ["pro", "geen", "contra"];
 var buttons = document.getElementsByClassName("stanceButtons");
-for(x = 0; x < stances.length; x++){
-    buttons[x].addEventListener("click", function(){answeredQuestion(stances[x]);});
+for(let x = 0; x < buttons.length; x++){
+    buttons[x].addEventListener("click", function(){answeredQuestion(this.id);});
 }
 document.getElementById("slaOver").addEventListener("click", function(){
     currentQuestion++;
